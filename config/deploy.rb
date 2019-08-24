6d4c0356adcc36130ebc0f72d10b2fc9ae11940e
Kuki1204 <okknjp1204@yahoo.co.jp>
2019-08-24 16:52:18 +0900

    deploy.rb modi

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,8 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  # keys: ['~/.ssh/kuki.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/kuki.pem']  
+                  # ※例：~/.ssh/key_pem.pem
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
